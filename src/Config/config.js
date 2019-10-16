76ca543eb60e23578dac5b85eb4a1921fb9989f8
bogdantigheci <41317794+bogdantigheci@users.noreply.github.com>
2019-10-16 14:55:40 +0300

    Update config.js

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,10 +1,10 @@
 export const DB_CONFIG = {
-  apiKey: "AIzaSyBj0sT-7NiXgIWZRvCdSKEBVacR8m8SVt0",
-  authDomain: "todoapp-14db1.firebaseapp.com",
-  databaseURL: "https://todoapp-14db1.firebaseio.com",
-  projectId: "todoapp-14db1",
-  storageBucket: "todoapp-14db1.appspot.com",
-  messagingSenderId: "310911672256",
-  appId: "1:310911672256:web:bfd7a0eecea6207f677225",
-  measurementId: "G-Z9YD4MZW1R"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
